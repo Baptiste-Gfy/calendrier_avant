10aa64cefa5c772a9c90fd7d678678f9b810f13d
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-09-30 15:06:33 +0200

    delete

--- header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let burger = document.querySelector(".burger");
-
-  burger.addEventListener("click", (e) => {
-    e.currentTarget.classList.toggle("open");
-    document.querySelector(".main-header").classList.toggle("open");
-  });
-});
