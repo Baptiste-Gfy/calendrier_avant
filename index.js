cf5768ee14a9bcdaa5dd895a145bbd0c070232c3
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-10-01 23:20:00 +0200

    added event to closed burger menu

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+// I'm waiting for the DOM to load to run the script.
+
 document.addEventListener("DOMContentLoaded", () => {
   let burger = document.querySelector(".burger");
 
   burger.addEventListener("click", (e) => {
+    let header = document.querySelector(".main-header");
+
     e.currentTarget.classList.toggle("open");
-    document.querySelector(".main-header").classList.toggle("open");
+
+    header.classList.toggle("open");
+  });
+
+  document.body.addEventListener("click", (e) => {
+    let header = document.querySelector(".main-header");
+    let burger = document.querySelector(".burger");
+
+    if (!e.target.classList.contains("burger")) {
+      header.classList.remove("open");
+      burger.classList.remove("open");
+    }
   });
 });
