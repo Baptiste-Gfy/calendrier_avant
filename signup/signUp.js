a77da87a18473a1873d2973d9e9f59baba3ae647
baptiste_gfy <geffroy.baptiste@outlook.fr>
2020-10-08 17:34:34 +0200

    test add event not found

---
+++ signup/signUp.js
@@ -0,0 +1,7 @@
+const checkbox = document.querySelector('.checkbox');
+
+    checkbox.addEventListener('clic',  function()) {
+  element.classList.remove('btn-disabled');
+  element.classList.add('btn-disabled');
+}
+    
