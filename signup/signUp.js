3711519eebc27d553a4157b56003effcc7d4d47c
baptiste_gfy <geffroy.baptiste@outlook.fr>
2020-10-08 23:15:45 +0200

    validation formulaire

--- signup/signUp.js
+++ signup/signUp.js
@@ -1,7 +1,22 @@
-const checkbox = document.querySelector('.checkbox');
+// recuperer input name,email,select
+// if name value not ""
+// if email value not ""
+// if select vallue not ""
 
-    checkbox.addEventListener('clic',  function()) {
-  element.classList.remove('btn-disabled');
-  element.classList.add('btn-disabled');
-}
-    
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const select = document.querySelector("#campus");
+const accepte = document.querySelector("#accepte");
+
+[name, email, campus, accepte].forEach((element) => {
+  element.addEventListener("change", () => {
+    const btn = document.querySelector("#jouer");
+    if (name.value && email.value && select.value !== "" && accepte.checked) {
+      btn.classList.remove("btn-disabled");
+      btn.classList.add("btn-valid");
+    } else {
+      btn.classList.remove("btn-valid");
+      btn.classList.add("btn-disabled");
+    }
+  });
+});
